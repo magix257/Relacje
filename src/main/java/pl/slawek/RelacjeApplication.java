b3940a080936dcc3198960cf4ef82cfb2ef374f3
magix257 <55533437+magix257@users.noreply.github.com>
2019-12-09 07:53:57 +0100

    Initial commit

---
+++ src/main/java/pl/slawek/RelacjeApplication.java
@@ -0,0 +1,13 @@
+package pl.slawek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelacjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelacjeApplication.class, args);
+	}
+
+}
