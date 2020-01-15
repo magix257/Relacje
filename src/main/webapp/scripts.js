2027894e0401c8eab4ccf497c340770a8f7d558e
magix257 <55533437+magix257@users.noreply.github.com>
2020-01-15 15:04:17 +0100

    zabawa

--- src/main/webapp/scripts.js
+++ src/main/webapp/scripts.js
@@ -132,7 +132,8 @@ function parseJSON() {
 	  if (this.readyState == 4 && this.status == 200) {
 	    var myObj = JSON.parse(this.responseText);
 	   
-	    document.getElementById("demo").innerHTML = myObj[0].colorName;
+	    document.getElementById("myTable").innerHTML = myObj[4].colorName;
+	   
 	   
 	  }
 	};
